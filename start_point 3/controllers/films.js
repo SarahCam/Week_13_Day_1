e5653edd195141863374bb5ecdc7f2dc3c1bf9b5
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-16 22:06:31 +0100

    Added get requests for /films and /films/:id

--- start_point 3/controllers/films.js
+++ start_point 3/controllers/films.js
@@ -6,8 +6,17 @@ var Review = require('../client/src/models/review');
 var express = require('express');
 var filmRouter = new express.Router();
 
+// This is a "get" request restful route for '/films',
+// which will respond with the JSON for the film at the
+// given index in the films array (id):
+filmRouter.get('/:id', function(req, res) {
+  res.json({data: films[req.params.id]});
+});
+
+// This is a "get" request restful route for '/films',
+// which will respond with the JSON for all films:
 filmRouter.get('/', function(req, res){
-  res.json({data: "Welcome to all films!"});
+  res.json(films);
 })
 
 module.exports = filmRouter;
