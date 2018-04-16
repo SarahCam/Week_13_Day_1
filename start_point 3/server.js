15ec2cf1db00e5a01ae671bd2a60640fdb48f605
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-16 21:19:49 +0100

    Added a use(require()) that uses the code in /controllers/index.js to intepret the restful routes responses, e.g. get

--- start_point 3/server.js
+++ start_point 3/server.js
@@ -4,9 +4,11 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// This will parse the static index.html file:
 app.use(express.static('client/build'));
 
-app.use(require('./controllers/index'));    // Added this to use index.js
+// This will use the code in index.js to interpret the restful routes:
+app.use(require('./controllers/index'));
 
 app.listen(3000, function () {
   console.log('App running on port '+this.address().port);
